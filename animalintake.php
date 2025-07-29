54f9f99b56eea3c0278cc0a1193fef5d15a3d9ea
Ratii <143424072+rati04@users.noreply.github.com>
2025-07-29 22:23:08 +0200

    Update animalintake.php

--- animalintake.php
+++ animalintake.php
@@ -1,47 +1,46 @@
 <html>
     <head>
         <title>
-            Adding dog records
+            Adding dog records CORRECT!
         </title>
     </head>
 
     <body>
-        <!-- to still be edited--> 
         <?php
 
-        include 'databaseconnection.php' ;
+        include 'databaseconnection.php' ; // include database connection file
 
-        $did = $_REQUEST['didnum'] ;
-        $dname = $_REQUEST['dname'] ;
-        $dgender = $_REQUEST['agender'] ;
-        $dspecies = $_REQUEST['aSpecies'] ;
-        $dage = $_REQUEST['dage'] ;
-        $dbreed = $_REQUEST['abreed'] ;
-        $darescueDetail = $_REQUEST['arescueDetails'] ;
-        $dbreed = $_REQUEST['dbreed'] ;
-        $dhealthStatus = $_REQUEST['ahealthStatus'] ;
-        $dweight = $_REQUEST['aweight'] ;
-        $dheight = $_REQUEST['aheight'] ;
-        $picture = $_FILES['picture']['name']; //image selection
-        
-        $destination = "dogs/".$picture ;
-        move_uploaded_file($_FILES['picture']['tmp_name'],$destination) ; //image uploaded
+        // getting form data:
+        $aId = $_REQUEST['aId'] ;
+        $aName = $_REQUEST['aName'] ;
+        $aSpecies = $_REQUEST['aSpecies'] ;
+        $aBreed = $_REQUEST['abreed'] ;
+        $aAge = $_REQUEST['aAge'] ;
+        $aGender = $_REQUEST['aGender'] ;
+        $aRescueDetail = $_REQUEST['aRescueDetail'] ;
+        $aHealthStatus = $_REQUEST['aHealthStatus'] ;
+        $aHeight = $_REQUEST['aHeight'] ;
+        $aWeight = $_REQUEST['aWeight'] ;
+        $apicture = $_FILES['aPicture']['name']; //image selection
+
+        $destination = "animal/".$apicture ; // will store images in the animals folder
+        move_uploaded_file($_FILES['aPicture']['tmp_name'],$destination) ; //image uploaded
 
         //sql to add to the database table:
-        $sql1="INSERT INTO dogsrecords(didnum, dname, agender, abreed, dage, dcolor, dheight, dweight, picture)
-                VALUES('$did','$dname','$dgender','$dbreed','$dage','$dcolor','$dheight','$dweight','$picture') ";
+        $sql1="INSERT INTO animalintake(animalId, aName, aSpecies, aBreed, aAge, aGender, aRescueDetails, aHealthStatus, aHeight, aWeight, aImage)
+                VALUES('$aId','$aName','$aSpecies','$aBreed','$aAge','$aGender','$aRescueDetail','$aHealthStatus','$aHeight','$aWeight','$apicture') ";
 
 
-        $result = $conn->query($sql1) ; // queery statement to execute SQL
-        
-        if($result ==  FALSE){
+        $result = $conn->query($sql1) ; // query statement to execute SQL
+
+        if($result ==  FALSE){          // if query fails
             die("unable to create the record".$conn->error) ;
 
         } else{
             echo "<br></br> Dog record successfullly created" ;
         }
 
-        $conn->close();
+        $conn->close(); 
 
             //menu options for navigating to other forms:
             echo"<p> <a href=\"AddDogForm.html\"> <button> Add another dog </button></a> </p>" ;
@@ -50,89 +49,14 @@
             echo"<p> <a href=\"dogRecordsearchform.html\"> <button> Search a record </button></a> </p>" ;
             echo"<p> <a href=\"loginForm.html\"> <button> Back to Login </button></a> </p>" ;
 
-        echo "</body>" ;
+        //echo "</body>" ;
 
     
 
-            
-            
-            
 
+        ?>
+    </body>
 
-
-
-
-        
-
-    
-
- 
-
-
-
-
-
-<?php
-    /*
-
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width ,initial-scale=1.0">
-    <title>Animal Intake and registration</title>
-    <link rel="stylesheet" href="Animalntake.css">
-</head>
-<body>
-    <form action="animalIntake.php" method="post">
-      <table>
-        <tr><th1 colspan="6">Animal Intake And Registration Form:</th1>
-        <tr><td>Animal ID:</td><td><input type="INT" value="" required></td></tr>
-        <tr><td>Name:</td><td><input type="text" size="30" value="" required></td></tr>
-        <tr><td>Species:</td><td><input type="text" size="30" value="" required></td></tr>
-        <tr></tr><td>Species:</td><td>
-            <select name="aSpecies" id="aSpecies" required>
-              <option value="">None</option>
-              <option value="Dog">Dog</option>
-              <option value="Donkey ">Donkey</option>
-              <option value="Cat">Cat</option>
-              <option value="Sheep">Sheep</option>
-              <option value="Pigs">Pigs</option>
-              <option value="Goat">Goat</option>
-              <option value="Horses">Horses</option>
-             </select></td></tr>
-        <tr></tr><td>Breed:</td><td>
-          <select name="aBreed" id="abreed" required >
-
-            <option value="">None</option>
-            <option value="Afferpinscher">Afferpinscher</option>
-            <option value="Aktla">Aktla</option>
-            <option value="Basset Hound">Basset Hound</option>
-            <option value="Beagle">Beagle</option>
-            <option value="Bernese Mountain Dog">Bernese Mountain Dog</option>
-            <option value="Bloodhound">Bloodhound</option>
-            <option value="Boerboel">Boerboel</option>
-            <option value="Border Collie">Border Colli</option>
-            <option value="German Shepherd">German Shepherd</option>
-            <option value="Labrador Retriever">Labrador Retriever</option>
-            <option value=" Rhodesian Ridgeback">Rhodesian Ridgeback</option></select>
-
-        <tr><td>Age:</td><td><input type="INT"  value="" required></td></tr>
-        <tr></tr><td>Gender:</td><td>
-          <select name="agender" id="agender" required >
-            <option value="Female">Female</option>
-            <option value="Male">Male</option>
-          </select>
-        
-        <tr><td>Rescue Details:</td><td><input type="text" size="70" value="" required></td></tr>
-        <tr><td>Health Status:</td><td><input type="text" size="20" value="" required></td></tr>
-        <tr><td>Height:</td><td><input type="INT"  value="" required></td></tr>
-        <tr><td>Weight:</td><td><input type="INT"  value="" required></td></tr>
-        <tr><td>Image:</td><td><input type="image" size="100" value="" required></td></tr>
-   
-      </table>
-</body>
 </html>
 
-    */
-</html>
+  
